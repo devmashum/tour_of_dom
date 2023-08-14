9332b9009f7f839f409a238770b7faf7027ac8ec
devmashum <dev.mashum@gmail.com>
2023-08-14 10:45:23 +0200

    new color

--- event/event.js
+++ event/event.js
@@ -2,8 +2,11 @@ const purpleButton = document.getElementById('make-purple');
 purpleButton.onclick = makePurple;
 function makePurple() {
     document.body.style.backgroundColor = 'purple';
-
-
 }
+const makeGray = document.getElementById('make-gray');
 
+makeGray.onclick = gray;
+function gray() {
+    document.body.style.backgroundColor = 'gray';
+}
 
